f0da59ca7fe2381a80d6ffe5efaf55283d6b1916
a.gerganov <gerganovalexander@gmail.com>
2023-07-04 11:26:12 +0300

    Updated Create Comment Operation Processor to work with booleans.

--- business/src/main/java/com/tinqin/academy/discussion/business/operations/createcomment/CreateCommentOperationProcessor.java
+++ business/src/main/java/com/tinqin/academy/discussion/business/operations/createcomment/CreateCommentOperationProcessor.java
@@ -3,7 +3,9 @@ package com.tinqin.academy.discussion.business.operations.createcomment;
 import com.tinqin.academy.discussion.data.models.Comment;
 import com.tinqin.academy.discussion.data.models.EntityType;
 import com.tinqin.academy.discussion.data.repositories.CommentRepository;
+import com.tinqin.academy.piim.api.game.existsbyid.ExistsByIdGameResult;
 import com.tinqin.academy.piim.api.game.getbyid.GetByIdGameResult;
+import com.tinqin.academy.piim.api.review.existsbyid.ExistsByIdReviewResult;
 import com.tinqin.academy.piim.api.review.getbyid.GetByIdReviewResult;
 import com.tinqin.academy.piim.restexport.PiimApiClient;
 import jakarta.persistence.EntityExistsException;
@@ -30,18 +32,17 @@ public class CreateCommentOperationProcessor implements CreateCommentOperation {
         // Either would fit better
         switch (input.getEntityType().toLowerCase()) {
             case "game":
-                Optional<GetByIdGameResult> gameType =
-                        Optional.ofNullable(piimApiClient.getGameById(input.getEntityId()));
-                if (gameType.isEmpty()) {
+                ExistsByIdGameResult doesGameExist = piimApiClient.checkIfGameExistsById(input.getEntityId());
+                if (!doesGameExist.getDoesExist()) {
                     throw new EntityNotFoundException(
                             String.format("Game with id %d does not exist.", input.getEntityId()));
                 }
             case "review":
-                Optional<GetByIdReviewResult> reviewType =
-                        Optional.ofNullable(piimApiClient.getReviewById(input.getEntityId()));
-                if (reviewType.isEmpty())
+                ExistsByIdReviewResult doesReviewExist = piimApiClient.checkIfReviewExistsById(input.getEntityId());
+                if (!doesReviewExist.getDoesExist()) {
                     throw new EntityNotFoundException(
-                            String.format("Game with id %d does not exist.", input.getEntityId()));
+                            String.format("Review with id %d does not exist.", input.getEntityId()));
+                    }
         }
         Comment comment = Comment.builder()
                 .comment(input.getComment())
