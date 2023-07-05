bcef6cc911a70cebcdd8482d74bcd0ee39360503
g.mihaylov <georgi.mihaylov23@gmail.com>
2023-07-05 12:06:32 +0300

    Fixed rest export

--- restexport/src/main/java/com/tinqin/academy/discussion/restexport/DiscussionApiClient.java
+++ restexport/src/main/java/com/tinqin/academy/discussion/restexport/DiscussionApiClient.java
@@ -2,8 +2,10 @@ package com.tinqin.academy.discussion.restexport;
 
 import com.tinqin.academy.discussion.api.operations.createcomment.CreateCommentInput;
 import com.tinqin.academy.discussion.api.operations.createcomment.CreateCommentResult;
+import com.tinqin.academy.discussion.api.operations.deleteallbyentityid.DeleteAllByEntityIdInput;
 import com.tinqin.academy.discussion.api.operations.deleteallbyentityid.DeleteAllByEntityIdResult;
 import com.tinqin.academy.discussion.api.operations.deletecommentbyid.DeleteCommentByIdResult;
+import com.tinqin.academy.discussion.api.operations.getallbyentityid.GetAllByEntityIdInput;
 import com.tinqin.academy.discussion.api.operations.getallbyentityid.GetAllByEntityIdResult;
 import com.tinqin.academy.discussion.api.operations.getcommentbyid.GetCommentByIdResult;
 import com.tinqin.academy.discussion.api.operations.update.UpdateCommentResult;
@@ -20,18 +22,14 @@ public interface DiscussionApiClient {
     @RequestLine("DELETE /api/comments/{id}")
     DeleteCommentByIdResult deleteCommentById(@Param Long id);
 
-    @RequestLine("DELETE /api/comments/delete-all-by-entityId?entityId={entityId}&entityType={entityType}")
-    DeleteAllByEntityIdResult deleteCommentsByEntity(@Param Long entityId, @Param String entityType);
+    @RequestLine("DELETE /api/comments/delete-all-by-entityId")
+    DeleteAllByEntityIdResult deleteCommentsByEntity(DeleteAllByEntityIdInput input);
 
     @RequestLine("POST /api/comments")
     CreateCommentResult createComment(CreateCommentInput createCommentInput);
 
-    @RequestLine(
-            """
-    GET /api/comments/get-all-by-entityId?entityId={entityId}&entityType={entityType}&page={page}&limit={limit}
-    """)
-    GetAllByEntityIdResult getAllCommentsByEntityId(
-            @Param Long entityId, @Param String entityType, @Param Integer page, @Param Integer limit);
+    @RequestLine("GET /api/comments/get-all-by-entityId")
+    GetAllByEntityIdResult getAllCommentsByEntityId(GetAllByEntityIdInput input);
 
     @RequestLine("GET /api/comments/{id}")
     GetCommentByIdResult getCommentById(@Param Long id);
